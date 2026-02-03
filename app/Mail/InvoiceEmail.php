520400bb1fe6d05a7fe3b332a9674900097b5223
Rico-Jetvendra <rico.jetvendra@gmail.com>
2026-02-03 16:27:24 +0700

    Mail

---
+++ app/Mail/InvoiceEmail.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Attachment;
+
+class InvoiceEmail extends Mailable{
+    use Queueable, SerializesModels;
+    public $data;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            // from: new Address('no-reply@pcr.com', 'No Reply'),
+            from: new Address(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME')),
+            subject: $this->data['subject'],
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'email.invoice_email',
+            with: $this->data
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        $attachments = [];
+
+        if(!empty($this->data['files'])){
+            foreach ($this->data['files'] as $key => $file) {
+                $attachments[] = Attachment::fromPath($file);
+            }
+        }
+
+        return $attachments;
+    }
+}
