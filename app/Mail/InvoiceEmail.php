792c7591327c0de90e38ff37b6eaddffd60b550e
Rico-Jetvendra <rico.jetvendra@gmail.com>
2026-02-03 16:29:29 +0700

    Invoice Email

--- app/Mail/InvoiceEmail.php
+++ app/Mail/InvoiceEmail.php
@@ -3,16 +3,13 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Mail\Mailables\Address;
-use Illuminate\Mail\Mailables\Attachment;
 
-class InvoiceEmail extends Mailable{
+class InvoiceEmail extends Mailable
+{
     use Queueable, SerializesModels;
+
     public $data;
 
     /**
@@ -24,43 +21,25 @@ class InvoiceEmail extends Mailable{
     }
 
     /**
-     * Get the message envelope.
+     * Build the message.
      */
-    public function envelope(): Envelope
+    public function build()
     {
-        return new Envelope(
-            // from: new Address('no-reply@pcr.com', 'No Reply'),
-            from: new Address(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME')),
-            subject: $this->data['subject'],
-        );
-    }
+        $mail = $this->from(
+                    env('MAIL_FROM_ADDRESS'),
+                    env('MAIL_FROM_NAME')
+                )
+                ->subject($this->data['subject'])
+                ->view('email.invoice_email')
+                ->with($this->data);
 
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'email.invoice_email',
-            with: $this->data
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        $attachments = [];
-
-        if(!empty($this->data['files'])){
-            foreach ($this->data['files'] as $key => $file) {
-                $attachments[] = Attachment::fromPath($file);
+        // Attach files if exist
+        if (!empty($this->data['files'])) {
+            foreach ($this->data['files'] as $file) {
+                $mail->attach($file);
             }
         }
 
-        return $attachments;
+        return $mail;
     }
 }
