a3239d083be35d83ededee50b6c63d367f35a7c3
Rico-Jetvendra <rico.jetvendra@gmail.com>
2026-02-04 14:47:10 +0700

    Email

--- app/Mail/ChangeStatus.php
+++ app/Mail/ChangeStatus.php
@@ -3,16 +3,13 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Mail\Mailables\Address;
 
-class ChangeStatus extends Mailable
+class ConfirmEmail extends Mailable
 {
     use Queueable, SerializesModels;
+
     public $data;
 
     /**
@@ -24,33 +21,18 @@ class ChangeStatus extends Mailable
     }
 
     /**
-     * Get the message envelope.
+     * Build the message.
      */
-    public function envelope(): Envelope
+    public function build()
     {
-        return new Envelope(
-            from: new Address(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME')),
-            subject: $this->data['subject'],
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'email.change_status',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this->from(
+                env('MAIL_FROM_ADDRESS'),
+                env('MAIL_FROM_NAME')
+            )
+            ->subject($this->data['subject'])
+            ->view('email.change_status')
+            ->with([
+                'data' => $this->data
+            ]);
     }
 }
--- app/Mail/ForgotPassword.php
+++ app/Mail/ForgotPassword.php
@@ -3,15 +3,13 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Mail\Mailables\Address;
 
-class ForgotPassword extends Mailable{
+class ForgotPassword extends Mailable
+{
     use Queueable, SerializesModels;
+
     public $data;
 
     /**
@@ -23,33 +21,18 @@ class ForgotPassword extends Mailable{
     }
 
     /**
-     * Get the message envelope.
+     * Build the message.
      */
-    public function envelope(): Envelope
+    public function build()
     {
-        return new Envelope(
-            from: new Address(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME')),
-            subject: $this->data['subject'],
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     */
-    public function content(): Content
-    {
-        return new Content(
-            view: 'email.forgot_password',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
+        return $this->from(
+                env('MAIL_FROM_ADDRESS'),
+                env('MAIL_FROM_NAME')
+            )
+            ->subject($this->data['subject'])
+            ->view('email.forgot_password')
+            ->with([
+                'data' => $this->data
+            ]);
     }
 }
